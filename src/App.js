cdcf864fbfee0d16c82515724cc52931570bb8b2
minseok <minseok13100@gmail.com>
2020-03-11 23:12:32 +0900

    nomadcoder reactjs 2.1 class

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function Food({ name }) {
   return <h3>I like {name} very much.</h3>;
 }
 
+const foodILike = ["kimchi", "hambuger", "pizza"];
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,9 @@ function App() {
       <Food name="kimchi" />
       <Food name="hambuger" />
       <Food name="pizza" />
+      {foodILike.map(food => (
+        <Food name={food} />
+      ))}
     </div>
   );
 }
