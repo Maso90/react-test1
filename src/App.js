1751c13aacd11d53c97ede7df884e37bacfe53c2
minseok <minseok13100@gmail.com>
2020-03-11 23:52:37 +0900

    nomadcoder reactjs 2.4 class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from "react";
+import Proptypes from "prop-types";
 
-function Food({ name }) {
-  return <h3>I like {name} very much.</h3>;
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h3>I like {name} very much.</h3>
+      <h4>score is {rating}</h4>
+    </div>
+  );
 }
 
-const foodILike = ["kimchi", "hambuger", "pizza"];
+Food.propTypes = {
+  name: Proptypes.string.isRequired,
+  rating: Proptypes.number
+};
+
+const foodILike = [
+  { id: 1, name: "kimchi", rating: 5 },
+  { id: 2, name: "hambuger", rating: 4.9 },
+  { id: 3, name: "pizza", rating: 3.7 }
+];
+
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} rating={dish.rating} />;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!!!</h1>
-      <Food name="kimchi" />
-      <Food name="hambuger" />
-      <Food name="pizza" />
-      {foodILike.map(food => (
-        <Food name={food} />
-      ))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
